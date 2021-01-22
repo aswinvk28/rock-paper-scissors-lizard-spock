ac412cc31a93659a4e4d5250e808678090cb4e10
Aswin Vijayakumar <aswinkvj@gmail.com>
2021-01-22 11:08:38 +0000

    build files added

---
+++ build/precache-manifest.d2210b584e351b801410f15e18771999.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3abc458c6010a952d29287c37c38a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e371b6cec9712ebf4344",
+    "url": "/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "79cb19984241d8248e52",
+    "url": "/static/css/main.a8ee109a.chunk.css"
+  },
+  {
+    "revision": "e371b6cec9712ebf4344",
+    "url": "/static/js/2.fc9ab29a.chunk.js"
+  },
+  {
+    "revision": "79cb19984241d8248e52",
+    "url": "/static/js/main.b836fa6b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
